bbde010e7bb6d35a5fa56a4289d73a7ba3818aa4
FredericGolling <ignatiuscorulux@protonmail.com>
2021-09-28 17:56:49 +0200

    changed SafeToDecimal.py into a function instead raw code

--- SafeToDecimal.py
+++ SafeToDecimal.py
@@ -1,16 +1,19 @@
-# hexadecimal string
-hex = ""
 
-# conversion
-dec = int(hex, 16)
+def hex(hex):
+    dec = int(hex, 16)
 
-print('Value in hexadecimal:', hex)
-print('Value in decimal:', dec)
+    print('Value in hexadecimal:', hex)
+    print('Value in decimal:', dec)
 
 
 
 
 
+def binary(binary):
+    dec = int(binary,2)
 
-binary = input()
-print(int(binary,2))
+    print('Value in Binary:', binary)
+    print('Value in decimal:', dec)
+
+binary('1010')
+hex('1010')
